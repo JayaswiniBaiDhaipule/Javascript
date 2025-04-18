a9e119a6069551c6ef9c65650d6dc518cff74d43
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2025-04-18 06:48:36 +0000

    Operations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,36 @@
+let score = 456
+console.log(typeof score);
+console.log(typeof(score));
+
+let scores = "456abc"
+console.log(typeof scores);
+console.log(typeof(scores));
+
+let value = Number(scores);
+console.log(typeof value);
+console.log(value);
+
+
+// "33 => 33"
+// "33and" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
