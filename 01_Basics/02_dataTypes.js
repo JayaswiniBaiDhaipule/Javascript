c3b9f1683de445bb16d45babb51be0cb713474dc
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2025-04-27 23:31:42 +0000

    dataTypes

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -27,3 +27,41 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+let x1 = 16 + "Volvo";
+console.log(x1);  //16Volvo  
+let x2 = "Volvo" + 16;
+console.log(x2); //Volvo16
+
+let x3 = 16 + 4 + "Volvo";
+console.log(x2);//20Volvo
+
+let x4 = "Volvo" + 16 + 4;
+console.log(x4); //Volvo164
+
+let x5 = 5;
+let y5 = 5;
+let z5 = 6;
+console.log(x5 == y5);      // Returns true
+console.log(x5 == z5);  // Returns false
