14cfa91db8e865124a96b51d78f1a75b015ca90a
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2025-04-27 22:55:20 +0000

    display properties methods

---
+++ 01_Basics/08_displayProperties.js
@@ -0,0 +1,19 @@
+// JavaScript can "display" data in different ways:
+
+// 1. Writing into an HTML element, using innerHTML or innerText.
+// 2. Writing into the HTML output using document.write().
+// 3. Writing into an alert box, using window.alert().
+// 4. Writing into the browser console, using console.log().
+
+document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+
+document.getElementById("demo").innerText = "Hello World";
+
+document.write(5 + 6);
+
+window.alert(5 + 6);
+
+alert(5 + 6);
+
+console.log(5 + 6);
+
