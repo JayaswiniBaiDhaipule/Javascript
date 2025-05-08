9a5882b7a19f1ed8d0f1501cfaaadf54ea6c2287
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2025-05-08 06:10:28 +0000

    Number as Objects

---
+++ 01_Basics/10_Number_as_Objects.js
@@ -0,0 +1,19 @@
+let x = 123;
+
+let y = new Number(123);
+
+console.log(x == y);
+
+console.log(x === y);
+
+
+let x1 = new Number(500);
+let y1 = new Number(500);
+console.log(x1 == y1);
+
+
+let x2 = new Number(500);
+let y2 = new Number(500);
+console.log(x2 === y2);
+
+/*Comparing two JavaScript objects always returns false.*/
